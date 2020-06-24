d2fc7d44d88601eaa3d211ea7cef834432133380
RitaHartnell <mel.soriano6123@gmail.com>
2020-06-24 15:03:35 -0400

    CRU but I can't test it so

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -16,6 +16,15 @@ class ArtistsController < ApplicationController
         redirect_to artist_path(@artist)
     end
 
+    def edit
+        @artist = Artist.find(params[:id])
+    end
+    
+    def update
+        @artist = Artist.find(params[:id])
+        @artist.update(artist_params)
+        redirect_to artist_path(@artist)
+    end
 
     private
 
